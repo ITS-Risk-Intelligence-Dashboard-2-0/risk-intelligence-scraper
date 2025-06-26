1575f148bd3d307dea9ec8edb5786eea05c51e0f
Kristan Seenath-Nagassar <snkris@umich.edu>
2025-06-26 09:57:38 -0400

    Feat: refactor

--- web_scraper_app/crawler/tasks.py
+++ web_scraper_app/crawler/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
 import requests
 from bs4 import BeautifulSoup
+from web_scraper_app.scraper.tasks import generate_pdf_task
 
 @shared_task
 def scrape_google_links():
     url = "https://www.google.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    links = soup.find_all("a")
-    hrefs = [link.get("href") for link in links if link.get("href")]
-    return hrefs
+    generate_pdf_task.delay(url)
+    #response = requests.get(url)
+    #soup = BeautifulSoup(response.text, "html.parser")
+    #links = soup.find_all("a")
+    #hrefs = [link.get("href") for link in links if link.get("href")]
+    #return hrefs
+    return []
