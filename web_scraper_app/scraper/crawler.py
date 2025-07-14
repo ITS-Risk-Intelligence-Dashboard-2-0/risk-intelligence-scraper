0398bb30630d8937923ae8fdc2d7214d0d7b9c07
Jiahui Chen <asdfjeff@m-w7gr2d7hjk.local>
2025-07-14 03:10:12 -0400

    added code to retrieve url and apply first two filters. untested

---
+++ web_scraper_app/scraper/crawler.py
@@ -0,0 +1,80 @@
+from celery import shared_task
+import requests
+from bs4 import BeautifulSoup
+from web_scraper_app.scraper.filter import filter_scraped_urls
+
+def same_domain(netloc1, netloc2):
+    if netloc1 == '' or netloc2 == '':
+        return True
+
+    domain_levels1 = netloc1.split('.')
+    domain_levels2 = netloc2.split('.')
+
+    try:
+        same_top_domain = domain_levels1[-1] == domain_levels2[-1]
+        same_second_domain = domain_levels1[-2] == domain_levels2[-2]
+    except:
+        return False
+
+    return same_top_domain and same_second_domain
+
+def analyze_last_path(chunk):
+    words = chunk.split("-")
+    if len(words) < 3:
+        return False
+
+    for word in words:
+        for c in word:
+            if c >= 'a' and c <= 'z':
+                continue
+
+            if c == '-' or c == '.':
+                continue
+
+            return False
+
+    return True
+
+def probably_news(path):
+    path_chunks = path.split("/")
+    is_article = analyze_last_path(path_chunks[-1])
+
+    if not is_article:
+        return None
+
+    return "/".join(path_chunks[:-1])
+
+@shared_task
+def scrape_google_links():
+    source_hubs = ["https://www.cnn.com", "https://www.foxnews.com"] # for testing purposes
+
+    sources = set(source_hubs)
+
+    while len(sources) > 0:
+        curr_source = sources.pop()
+
+        curr_source_parsed = urlparse(curr_source)
+
+        try:
+            response = requests.get(curr_source)
+        except:
+            continue
+
+        parsed_html = BeautifulSoup(response.text, "html.parser")
+
+        anchor_tags = soup.find_all("a")
+
+        for anchor in anchors:
+            if not anchor.has_attr("href"):
+                continue
+
+            scraped_url = urlparse(anchor.get("href"))
+            if not same_domain(scraped_url.netloc, curr_source_parsed.netloc):
+                continue
+
+            if probably_news(scraped_url.path):
+                final_url = build_url(curr_source_parsed, scraped_url)
+                filter_scraped_urls.delay(final_url)
+
+    return []
+
